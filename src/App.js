d31c64710847514ba953e8f7538f0062ae26a5c7
maisamaximo <maisa_maximo@outlook.com>
2019-05-08 00:23:19 -0300

    adicionado o FourSquace para places API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { scrypt } from 'crypto';
+import axios from 'axios'
 
 class App extends React.Component { 
 
+  state = {
+    venues: []
+  }
+
   componentDidMount() {
     this.renderMap()
+    this.getVenues()
   }
 
   renderMap = () => {
@@ -27,6 +32,26 @@ class App extends React.Component {
         <div id="map"></div>
       </main>
     );
+  }
+
+  getVenues = () => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore"
+    const parameters = {
+      client_id: "ZHCS2GW54301UNBEQ1YWXYUV3A1UYJ003HZXMBFKGI5DMMQV",
+      client_secret: "N0B1NI4MEBCVXKOOYXREGV1KBDTS3NRC30ILOHXGZ1W123PT",
+      query: "food",
+      near: "São Paulo",
+      v: "201905"
+    }
+    axios.get(endPoint + new URLSearchParams(parameters))
+    .then(response => {
+      this.setState({
+        venues: response.data.response.group[0].items
+      })
+    })
+    .catch(error => {
+      console.log("error " + error)
+    })
   }
 }
 
